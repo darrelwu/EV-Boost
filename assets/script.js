77ff9644eccd922481dad3d3e04a18b2a809fea3
mkdemon5 <alvaradomike5@gmail.com>
2022-09-15 19:24:53 -0500

    isolate elements of api request

--- assets/script.js
+++ assets/script.js
@@ -19,10 +19,16 @@ function initMap() {
   let url = 
   "https://api.openchargemap.io/v3/poi?key=4d0b1b45-2674-481a-96b8-a48760d39238/?output=json&boundingbox=(30.513171,-97.936058),(30.121121,-97.567030)&maxresults=500"
   
-  fetch (url).then(function(response){
-   console.log(response.json());
-//    const evList = JSON.parse(response.getItem());
+  fetch (url)
+  .then((response) => response.json())
+  .then((response) => {
+    const evList = response;
+    console.log(response);
+    console.log(response.length);
 
-//    console.log(evList[0][0][10]);
+    console.log(evList[0].AddressInfo.Postcode);
+
+    
+
   });
 
