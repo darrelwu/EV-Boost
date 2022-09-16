d86430548c56881d3f7258b026763697a17936af
mkdemon5 <alvaradomike5@gmail.com>
2022-09-15 23:12:42 -0500

    fixed recentering issue

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,11 @@ const mapMarker =[];
 var evList;
 var markMe;
 var map;
-var lat = 30.2747;
-var lng = -97.7404;
 
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: lat, lng: lng };
+    const uluru = { lat: 30.2747, lng: -97.7404 };
     // The map, centered at Uluru
     map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
@@ -24,16 +22,14 @@ function initMap() {
 window.initMap = initMap;
 
 var submitBtn = document.querySelector('#submitMe');
-
 submitBtn.addEventListener('click', zipStorage);
-
 
 function zipStorage(event) {
   event.preventDefault();
   
   var zip_code = document.getElementById("zip_code").value;
   localStorage.setItem ("Is_zipcode", zip_code);
-  map.setCenter(-96.6404,31.1747,12);
+  
   mapMarkerCreator();
 }
 
@@ -44,10 +40,9 @@ fetch (url)
 .then((response) => response.json())
 .then((response) => {
   evList = response;
-  console.log(response);
-  console.log(response.length);
-
-  console.log(evList[0].AddressInfo.Postcode);
+  // console.log(response);
+  // console.log(response.length);
+  // console.log(evList[0].AddressInfo.Postcode);
   });
 
 function mapMarkerCreator() {
@@ -61,14 +56,11 @@ function mapMarkerCreator() {
 
       mapMarker.push(myLatLong);
     }
-
     i++;
   }
 
-  console.log(mapMarker);
-  // map.setCenter(lng,lat,12);
-  // initMap();
   if (mapMarker.length>0) {
+    map.setCenter({lat: mapMarker[0][0],lng: mapMarker[0][1]});
     for (var j=0; j<mapMarker.length; j++) {
       const markMe = { lat: mapMarker[j][0], lng: mapMarker[j][1]};
       new google.maps.Marker({
@@ -76,24 +68,6 @@ function mapMarkerCreator() {
         map: map,
       });
     }
-    var newCenter = {lat: mapMarker[0][0], lng: mapMarker[0][1]};
-
-    // map = new google.maps.Map(document.getElementById("map"), {
-    //   zoom: 12,
-    //   center: uluru,
-    // });
-    lat = mapMarker[0][0];
-    lng = mapMarker[0][1];
-
-    
-    
-
-
-    
-    // return lat,lng;
   }
-  console.log(lat);
-  console.log(lng);
-
 }
 
